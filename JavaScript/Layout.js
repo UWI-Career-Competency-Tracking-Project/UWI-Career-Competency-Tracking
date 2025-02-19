66a85f8d48438f5ba69d9a07aaa6c284754f1262
Josiah Phillip <josiahphillip72@gmail.com>
2025-02-19 13:29:29 -0400

    new Layout and dashboard

--- JavaScript/Layout.js
+++ JavaScript/Layout.js
@@ -1,19 +1,4 @@
 document.addEventListener("DOMContentLoaded", () => {
-    // Sidebar toggle functionality
-    const toggleButton = document.querySelector(".toggle-sidebar-btn");
-    const sidebar = document.querySelector(".sidebar");
-    toggleButton.addEventListener("click", () => {
-        sidebar.classList.toggle("show");
-    });
-
-    // Highlight active sidebar link
-    const sidebarLinks = document.querySelectorAll(".sidebar ul li a");
-    const currentPage = window.location.pathname.split("/").pop();
-    sidebarLinks.forEach((link) => {
-        if (link.getAttribute("href") === currentPage) {
-            link.classList.add("active");
-        }
-    });
 
     // Profile dropdown functionality with smooth slide
     const dropdown = document.querySelector(".dropdown");
