e7e46b614485f3cab87362739e974dbe939366bb
Josiah Phillip <josiahphillip72@gmail.com>
2025-03-03 10:21:17 -0400

    Dashhboard update

---
+++ App/static/JavaScript/loader.js
@@ -0,0 +1,12 @@
+var l = document.getElementById("loader");
+
+window.addEventListener("load", function(){
+  // Wait 2 seconds after the page loads
+  setTimeout(function(){
+    l.style.opacity = "0"; // Start fade out (1s transition defined in CSS)
+    // After fade-out transition is done, hide the loader completely
+    setTimeout(function(){
+      l.style.display = "none";
+    }, 1000);
+  }, 2000);
+});
