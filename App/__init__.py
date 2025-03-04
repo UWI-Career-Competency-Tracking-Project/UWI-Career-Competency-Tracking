1a28b9808b4f16d12049cb271798ba8d25fe5253
IjaazSisarran <ijaazsisarran@gmail.com>
2025-03-04 13:20:23 -0400

    Small fixes

--- App/__init__.py
+++ App/__init__.py
@@ -27,7 +27,7 @@ def create_app():
     
     db.init_app(app)
     login_manager.init_app(app)
-    login_manager.login_view = 'auth_views.login_action'
+    login_manager.login_view = 'auth_views.login'
     login_manager.login_message = 'Please log in to access this page.'
     login_manager.login_message_category = 'info'
     
